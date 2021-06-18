5d0d1a89dc490ed9ca95257f61a1605d71e0bb38
jayesh-hertzai <jayesh.mirani@students.vnit.ac.in>
2021-06-18 21:35:38 +0530

    changed main

--- practice/src/App.js
+++ practice/src/App.js
@@ -2,9 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <h1>HELLO TEAM </h1>
-  );
+  return <h1>HELLO TEAM from main </h1>;
 }
 
 export default App;
